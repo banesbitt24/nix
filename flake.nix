79e121f7ccd333c318083906088d73875e4b89e8
Brandon Nesbitt <brandon.nesbitt@fastmail.com>
2024-12-26 09:06:59 -0700

    add x11 module

--- flake.nix
+++ flake.nix
@@ -10,30 +10,38 @@
     distro-grub-themes.url = "github:AdisonCavani/distro-grub-themes";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
-    nixosConfigurations.greywolf = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
-      modules = [
-        ./modules/nixos/bootloader.nix
-        ./modules/nixos/configuration.nix
-        ./modules/nixos/fonts.nix
-        ./modules/nixos/keymap.nix
-        ./modules/nixos/locale.nix
-        ./modules/nixos/network.nix
-        ./modules/nixos/packages.nix
-        ./modules/nixos/plasma.nix
-        ./modules/nixos/print.nix
-        ./modules/nixos/services.nix
-        ./modules/nixos/sound.nix
-        ./modules/nixos/time.nix
-        ./modules/nixos/users.nix
-        ./modules/nixos/virt.nix
-        inputs.distro-grub-themes.nixosModules.x86_64-linux.default
-        home-manager.nixosModules.default
-        {
-          home-manager.backupFileExtension = "hmb";
-        }
-      ];
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }@inputs:
+    {
+      nixosConfigurations.greywolf = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./modules/nixos/bootloader.nix
+          ./modules/nixos/configuration.nix
+          ./modules/nixos/fonts.nix
+          ./modules/nixos/keymap.nix
+          ./modules/nixos/locale.nix
+          ./modules/nixos/network.nix
+          ./modules/nixos/packages.nix
+          ./modules/nixos/plasma.nix
+          ./modules/nixos/print.nix
+          ./modules/nixos/services.nix
+          ./modules/nixos/sound.nix
+          ./modules/nixos/time.nix
+          ./modules/nixos/users.nix
+          ./modules/nixos/virt.nix
+          ./modules/nixos/x11.nix
+          inputs.distro-grub-themes.nixosModules.x86_64-linux.default
+          home-manager.nixosModules.default
+          {
+            home-manager.backupFileExtension = "hmb";
+          }
+        ];
+      };
     };
-  };
 }
