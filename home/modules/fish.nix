8ad8b3e8dc5deca747e4b729c6b991cb787cc546
Brandon Nesbitt <brandon.nesbitt@protonmail.com>
2025-09-24 15:13:13 -0600

    add lazydocker and lazygit aliases to fish

--- home/modules/fish.nix
+++ home/modules/fish.nix
@@ -3,29 +3,31 @@
 {
   programs.fish = {
     enable = true;
-    
+
     interactiveShellInit = ''
       # Disable greeting
       set fish_greeting
-      
+
       # Enable starship prompt
       starship init fish | source
     '';
-    
+
     shellAliases = {
       ll = "ls -l";
       la = "ls -la";
       ".." = "cd ..";
       "..." = "cd ../..";
       grep = "grep --color=auto";
-      
+      lg = "lazygit";
+      ld = "lazydocker";
+
       # Nix-specific aliases
       rebuild = "sudo nixos-rebuild switch --flake ~/.nix#quicksilver";
       rebuild-home = "home-manager switch --flake ~/.nix#quicksilver";
       update-flake = "nix flake update ~/.nix";
       nix-gc = "sudo nix-collect-garbage -d && nix-collect-garbage -d";
     };
-    
+
     functions = {
       # Function to quickly edit nix configs
       nixconf = {
