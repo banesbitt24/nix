a8db1f44f7a08251836ca56327e29ff54a73120a
Brandon Nesbitt <brandon.nesbitt@protonmail.com>
2025-10-24 05:53:16 -0600

    wallpaper udpate

--- home/modules/hyprpaper.nix
+++ home/modules/hyprpaper.nix
@@ -11,11 +11,11 @@
       splash_offset = 2.0;
 
       preload = [
-        "/home/brandon/.nix/wallpapers/nixos.png"
+        "/home/brandon/.nix/wallpapers/mountain_jaws.jpg"
       ];
 
       wallpaper = [
-        ",/home/brandon/.nix/wallpapers/nixos.png"
+        ",/home/brandon/.nix/wallpapers/mountain_jaws.jpg"
       ];
     };
   };
